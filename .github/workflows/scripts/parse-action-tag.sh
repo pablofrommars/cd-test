9d3e3589118e28b21675d76c7c3a50cff2313568
pablofrommars <pablo.belin@wearelevels.com>
2023-06-17 13:39:11 +0200

    fix11

--- .github/workflows/scripts/parse-action-tag.sh
+++ .github/workflows/scripts/parse-action-tag.sh
@@ -1,15 +1,25 @@
 #! /bin/bash
+base_ref=$1
+tag=$2
 
-tag=$1
+base_ref_pattern="^refs/heads/(.*)$"
 
-pattern="^refs/tags/release/(stage|prod)/(major|minor|patch|build)$"
+if [[ $base_ref =~ $base_ref_pattern ]]; then
+	branch=${BASH_REMATCH[1]}
 
-if [[ $tag =~ $pattern ]]; then
+	echo "BRANCH=${branch}" >> "$GITHUB_OUTPUT"
+else 
+	exit 1
+fi
+
+tag_pattern="^refs/tags/release/(stage|prod)/(major|minor|patch|build)$"
+
+if [[ $tag =~ $tag_pattern ]]; then
 	tag=${BASH_REMATCH[0]}
 	action=${BASH_REMATCH[2]}
 	target=${BASH_REMATCH[1]}
 
-	echo "Bump ${action} for ${target} configured" >> $GITHUB_STEP_SUMMARY
+	echo "Bump ${action} for ${target} configured on ${branch}" >> $GITHUB_STEP_SUMMARY
 
 	echo "TAG=${tag}" >> "$GITHUB_OUTPUT"
 	echo "ACTION=${action}" >> "$GITHUB_OUTPUT"
