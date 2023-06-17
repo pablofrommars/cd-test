85c6bf8b629857d47cae158316b9e57d5f52a4ed
pablofrommars <pablo.belin@wearelevels.com>
2023-06-17 09:40:26 +0200

    test

--- .github/workflows/scripts/get-version.sh
+++ .github/workflows/scripts/get-version.sh
@@ -2,7 +2,9 @@
 
 ref=$(git for-each-ref --sort=-creatordate --count 1 --format="%(refname:short)" "refs/tags/v*")
 
-[[ $ref =~ "v(.*)" ]]
+[[ $ref =~ "^v(.*)$" ]]
 version=${BASH_REMATCH[1]}
 
+echo "Version ${version} retrieved"
+echo "Version ${version} retrieved" >> $GITHUB_STEP_SUMMARY
 echo "VERSION=${version}" >> "$GITHUB_OUTPUT"
