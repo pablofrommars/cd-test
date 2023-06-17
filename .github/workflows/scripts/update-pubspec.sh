ecd45549ccfe590a5c52fb49580246061b4b6e34
pablofrommars <pablo.belin@wearelevels.com>
2023-06-17 12:51:11 +0200

    update

--- .github/workflows/scripts/update-pubspec.sh
+++ .github/workflows/scripts/update-pubspec.sh
@@ -9,4 +9,4 @@ sed -i "s/^version: .*$/version: ${version}/" pubspec.yaml
 git add pubspec.yaml 
 
 git -c user.name="${user_name}" -c user.email="${user_email}" \
-	commit -m "Update pubspec version to ${{version}}"
+	commit -m "Update pubspec version to ${version}"
